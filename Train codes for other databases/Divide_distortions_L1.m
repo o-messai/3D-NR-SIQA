dd41961fa60acb0ed38350676b70941105ea595e
Oussama Messai <mr.oussama@aol.com>
2022-06-16 14:52:12 +0200

    latest version

---
+++ Train codes for other databases/Divide_distortions_L1.m
@@ -0,0 +1,12 @@
+function [JPEG, JP2K, WN, BLUR, FF] = Divide_distortions_L1(input)
+
+
+WN = input([161:240]);   
+JP2K = input([1:80]);   
+JPEG = input([81:160]); 
+BLUR = input([241:285]);  
+FF = input([286:365]);
+  
+
+end 
+
